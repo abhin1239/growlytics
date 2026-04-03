6de94c7f8997355bd6bfbac3ca865a4cc70f5173
abhin1239 <143788081+abhin1239@users.noreply.github.com>
2026-04-03 09:35:05 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -10,8 +10,7 @@ from functools import wraps
 import time
 
 from flask import Flask, request, jsonify, g, send_from_directory
-from flask_cors import COR
-CORS(app, origins=["https://growlytics-six.vercel.app/"])
+from flask_cors import CORS(app, origins=["https://growlytics-six.vercel.app/"])
 from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
 
